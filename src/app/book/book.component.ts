a5294126d9441a9e8cddc76b8ce8ee090919dc7b
Greg Yingling <19862719+malcalevak@users.noreply.github.com>
2021-01-22 22:04:11 -0500

    Pagination "fixes"

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -13,14 +13,15 @@ export class BookComponent {
   @Input() books: Book[] = new Array;
   @Output() pageEvent = new EventEmitter<PageEvent>();
 
-  totalResults = 1814;//needs actual value
+  totalResults = 11;//this.books.length + 1;//not returned from API, if we get 10 results, assume another page
   responseTime = 12;//needs actual value
 
   pageSize = 10;
   pageIndex = 0;
-  showFirstLastButtons = true;
 
   handlePageEvent(event: PageEvent) {
     this.pageEvent.emit(event);
+    this.pageIndex = event.pageIndex;
+    this.totalResults = event.pageIndex*10 + this.books.length + 1
   }
 }
